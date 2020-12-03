ec23198e62c9b50fb24cf04a49169b298ed3cd7f
tylercarberry <tylercarberry@gmail.com>
2020-12-03 18:06:10 -0500

    Run tests on github actions

--- test.py
+++ test.py
@@ -1,25 +1,13 @@
-import requests
-import main
-import os
+import unittest
+from app import app
 
 
-def test_episode_495():
-    temp_files = ["episode.mp3", "db.pklz"]
-    for f in temp_files:
-        if os.path.isfile(f):
-            os.remove(f)
-
-    r = requests.get("https://media.blubrry.com/thisisonlyatest/d2rormqr1qwzpz.cloudfront.net/podcast/thisisonlyatest_20190411.mp3")
-
-    with open(main.PODCAST_FILE_NAME, 'wb') as f:
-        f.write(r.content)
-        f.close()
-
-    res = main.generate_timestamps()
-
-    expected = ["Intro", "Top Story", "Pop Culture News", "Technology News", "Moment of Science", "Pinball", "VR Minute", "Things That Annoy Me", "Outro"]
-
-    assert len(expected) == len(res)
+class BasicTestCase(unittest.TestCase):
+    def test_home(self):
+        tester = app.test_client(self)
+        response = tester.get('/', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
 
 
-test_episode_495()
+if __name__ == '__main__':
+    unittest.main()
