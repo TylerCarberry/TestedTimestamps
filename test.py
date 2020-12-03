7f1e7cd294edc6825b491247eea2d81d9d37229a
tylercarberry <tylercarberry@gmail.com>
2020-12-03 18:11:24 -0500

    test failing test

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ class BasicTestCase(unittest.TestCase):
     def test_home(self):
         tester = app.test_client(self)
         response = tester.get('/', content_type='html/text')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 201)
 
 
 if __name__ == '__main__':
