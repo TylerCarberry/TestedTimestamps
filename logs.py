10eeb82b249d1488fa863a7e9cafb88ad8d2ca50
tylercarberry <tylercarberry@gmail.com>
2020-12-18 00:31:00 -0500

    fix apprise format

--- logs.py
+++ logs.py
@@ -1,10 +1,11 @@
 import apprise
 import os
 
+CUSTOM_AVATAR = False
 
 def post_to_discord(title, body):
     app = apprise.Apprise()
-    app.add("discord://{}/{}/".format(os.environ["DISCORD_WEBHOOK_ID"], os.environ["DISCORD_WEBHOOK_TOKEN"]))
+    app.add("discord://{}/{}/?avatar={}".format(os.environ["DISCORD_WEBHOOK_ID"], os.environ["DISCORD_WEBHOOK_TOKEN"], CUSTOM_AVATAR))
     app.notify(
         title=title,
         body=body
