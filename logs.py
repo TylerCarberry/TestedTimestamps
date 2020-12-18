8beb4170622b664a650a192e668d3b5b60d6d602
tylercarberry <tylercarberry@gmail.com>
2020-12-18 00:58:24 -0500

    fix crash "no current event loop"

--- logs.py
+++ logs.py
@@ -1,9 +1,12 @@
+import asyncio
+
 import apprise
 import os
 
 CUSTOM_AVATAR = False
 
 def post_to_discord(title, body):
+    asyncio.set_event_loop(asyncio.new_event_loop())
     app = apprise.Apprise()
     app.add("discord://{}/{}/?avatar={}".format(os.environ["DISCORD_WEBHOOK_ID"], os.environ["DISCORD_WEBHOOK_TOKEN"], CUSTOM_AVATAR))
     app.notify(
